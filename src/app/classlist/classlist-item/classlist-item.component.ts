ca6f731db5841f2e2f01376e8cbd20c48ffbee1a
Andy Wildisen <awildisen@gmx.ch>
2017-10-09 22:33:57 +0200

    ng structure

---
+++ src/app/classlist/classlist-item/classlist-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-classlist-item',
+  templateUrl: './classlist-item.component.html'
+})
+export class ClasslistItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
