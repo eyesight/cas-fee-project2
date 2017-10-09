4ae82f755a8817dd46ae91731325b906cca8ad91
Andy Wildisen <awildisen@gmx.ch>
2017-10-09 22:25:25 +0200

    ng structure set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatMessageComponent } from './chat/chat-message/chat-message.component';
+import { ChatAddmessageComponent } from './chat/chat-addmessage/chat-addmessage.component';
+import { ChatThreadComponent } from './chat/chat-thread/chat-thread.component';
+import { ClasslistItemComponent } from './classlist/classlist-item/classlist-item.component';
+import { ClasslistListComponent } from './classlist/classlist-list/classlist-list.component';
+import { ProfileAvatarComponent } from './profile/profile-avatar/profile-avatar.component';
+import { ProfileFormComponent } from './profile/profile-form/profile-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatMessageComponent,
+    ChatAddmessageComponent,
+    ChatThreadComponent,
+    ClasslistItemComponent,
+    ClasslistListComponent,
+    ProfileAvatarComponent,
+    ProfileFormComponent
   ],
   imports: [
     BrowserModule
