45af61d44382f46fa61efc702853a4b88c4b7c74
Andy Wildisen <awildisen@gmx.ch>
2017-10-10 14:14:07 +0200

    ng structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppComponent } from './app.component';
 import { ChatMessageComponent } from './30_chat/chat-message/chat-message.component';
 import { ChatAddmessageComponent } from './30_chat/chat-addmessage/chat-addmessage.component';
 import { ChatThreadComponent } from './30_chat/chat-thread/chat-thread.component';
-import { ClasslistItemComponent } from './classlist/classlist-item/classlist-item.component';
-import { ClasslistListComponent } from './classlist/classlist-list/classlist-list.component';
+import { ClasslistItemComponent } from './20_classlist/classlist-item/classlist-item.component';
+import { ClasslistListComponent } from './20_classlist/classlist-list/classlist-list.component';
 import { ProfileAvatarComponent } from './10_profile/profile-avatar/profile-avatar.component';
 import { ProfileFormComponent } from './10_profile/profile-form/profile-form.component';
 
