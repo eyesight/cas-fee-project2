a59da7fd253373b3cb6c86fb38cd61ba545ad953
Andy Wildisen <awildisen@gmx.ch>
2017-10-10 14:02:32 +0200

    ng structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ChatMessageComponent } from './chat/chat-message/chat-message.component';
-import { ChatAddmessageComponent } from './chat/chat-addmessage/chat-addmessage.component';
-import { ChatThreadComponent } from './chat/chat-thread/chat-thread.component';
+import { ChatMessageComponent } from './30_chat/chat-message/chat-message.component';
+import { ChatAddmessageComponent } from './30_chat/chat-addmessage/chat-addmessage.component';
+import { ChatThreadComponent } from './30_chat/chat-thread/chat-thread.component';
 import { ClasslistItemComponent } from './classlist/classlist-item/classlist-item.component';
 import { ClasslistListComponent } from './classlist/classlist-list/classlist-list.component';
-import { ProfileAvatarComponent } from './profile/profile-avatar/profile-avatar.component';
-import { ProfileFormComponent } from './profile/profile-form/profile-form.component';
+import { ProfileAvatarComponent } from './10_profile/profile-avatar/profile-avatar.component';
+import { ProfileFormComponent } from './10_profile/profile-form/profile-form.component';
 
 @NgModule({
   declarations: [
