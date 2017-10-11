d62980711a36efb9923161721adbf62d61063de3
Andy Wildisen <awildisen@gmx.ch>
2017-10-11 22:21:07 +0200

    routing

---
+++ src/app/app.routes.ts
@@ -0,0 +1,25 @@
+/**
+ * Created by awedag on 11.10.17.
+ */
+/*
+import { Routes } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { NoContentComponent } from './no-content';
+*/
+/*
+export const ROUTES: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, data: { title: 'Home' } },
+  { path: '10_profile', loadChildren: './+todo-advanced-directive#TodoAdvancedDirectiveModule', data: { title: 'ClassList' } },
+  {
+    path: 'chat', loadChildren: './+todo-advanced-errorhandling#TodoAdvancedErrorhandlingModule',
+    data: { title: 'Errorhandling' }
+  },
+  {
+    path: 'chat', loadChildren: './+todo-advanced-errorhandling#TodoAdvancedErrorhandlingModule',
+    data: { title: 'Errorhandling' }
+  },
+  { path: '**', component: NoContentComponent },
+];
+*/
