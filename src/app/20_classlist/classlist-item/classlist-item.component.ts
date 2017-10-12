ecc2cf2c1df5d568ff2c977b0d2c39635b07d8c0
Andy Wildisen <awildisen@gmx.ch>
2017-10-12 20:34:21 +0200

    tsconfig using app as root-dir -> easy import

--- src/app/20_classlist/classlist-item/classlist-item.component.ts
+++ src/app/20_classlist/classlist-item/classlist-item.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Message } from 'app/model/'
 @Component({
   selector: 'app-classlist-item',
   templateUrl: './classlist-item.component.html'
 })
 export class ClasslistItemComponent implements OnInit {
 
+  message: Message;
   constructor() { }
 
   ngOnInit() {
--- src/app/20_classlist/classlist-list/classlist-list.component.ts
+++ src/app/20_classlist/classlist-list/classlist-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from 'app/model/message.model';
 
 @Component({
   selector: 'app-classlist-list',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClasslistListComponent implements OnInit {
 
+  messages: Message[] = [];
   constructor() { }
 
   ngOnInit() {
