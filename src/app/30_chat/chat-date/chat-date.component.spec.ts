4f1874cec0e62631fc50450c428c1b9861145da8
Andy Wildisen <awildisen@gmx.ch>
2017-10-19 19:34:46 +0200

    date

---
+++ src/app/30_chat/chat-date/chat-date.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChatDateComponent } from './chat-date.component';
+
+describe('ChatDateComponent', () => {
+  let component: ChatDateComponent;
+  let fixture: ComponentFixture<ChatDateComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ChatDateComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChatDateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
