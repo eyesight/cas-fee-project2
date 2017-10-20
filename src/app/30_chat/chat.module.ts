ba7f9195aa546fbd94e713d688a93edc94977271
Claudia Friedrich <claudia.friedrich@nsgroup.ch>
2017-10-20 18:17:19 +0200

    merge

--- src/app/30_chat/chat.module.ts
+++ src/app/30_chat/chat.module.ts
@@ -4,7 +4,9 @@ import { CommonModule } from '@angular/common';
 import { MessageItemService } from '../services/messageItem.service';
 import { ChatAddmessageComponent } from './chat-addmessage/chat-addmessage.component';
 import { ChatMessageComponent } from './chat-message/chat-message.component';
+/*
 import { ChatDateComponent } from './chat-date/chat-date.component';
+*/
 import { ChatThreadComponent } from './chat-thread/chat-thread.component';
 import { ChatComponent } from './chat.component';
 import { ChatRoutesModule } from './chat.routes';
@@ -13,7 +15,7 @@ import { ChatRoutesModule } from './chat.routes';
   imports: [
     CommonModule, ChatRoutesModule
   ],
-  declarations: [ChatComponent, ChatAddmessageComponent, ChatMessageComponent, ChatThreadComponent, ChatDateComponent],
+  declarations: [ChatComponent, ChatAddmessageComponent, ChatMessageComponent, ChatThreadComponent],
   providers: [MessageItemService]
 })
 export class ChatModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,3 @@
-/**
- * Created by awedag on 11.10.17.
- */
-
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
