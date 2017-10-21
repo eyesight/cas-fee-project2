06be957f725d0827e12d3ce9bef7084b8351e766
Claudia Friedrich <claudia.friedrich@nsgroup.ch>
2017-10-21 15:27:58 +0200

    improved displaying/removing header/footer when logged in or out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './_services/authentication.service';
 import { Route } from '@angular/router';
 
 import { ROUTES } from './app.routes';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
@@ -11,8 +13,9 @@ export class AppComponent {
 
  // public navItems: Route[];
 
-  constructor(){
-   // this.navItems = ROUTES.filter((route) => route.data);
-
+  constructor(
+    public authService: AuthenticationService
+  ) {
+    // this.navItems = ROUTES.filter((route) => route.data);
   }
 }
