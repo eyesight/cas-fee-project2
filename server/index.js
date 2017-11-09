06844feb6d02f23d58a0969bfd6b1656e54d55fa
Andy Wildisen <awildisen@gmx.ch>
2017-11-09 18:15:48 +0100

    allow auth. in header

--- server/index.js
+++ server/index.js
@@ -25,14 +25,14 @@ store.doQuery();
 //dbchat.getAllMessages('hansli@example.com',null);
 
 app.use(function(req, res, next) {
- // res.header("Access-Control-Allow-Origin", "*");
- // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Accept");
+
+ /* res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept,Authorization, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-
+*/
   next();
 });
 
