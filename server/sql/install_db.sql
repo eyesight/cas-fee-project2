558b51a825fa5ec6dd3ea8eb6c883667b79d2944
Andy Wildisen <awildisen@gmx.ch>
2017-11-12 17:48:35 +0100

    db

--- server/sql/install_db.sql
+++ server/sql/install_db.sql
@@ -76,3 +76,22 @@ CREATE TABLE `users` (
 INSERT INTO `users` VALUES(1, '1', 'hansli@example.com', '$2a$11$k8EkgulVtkQxuD8Jt.xyUeg1wNKivBHbluVyI98vnFpt9.SJsx1XK', 'Hansli', 'Schneider', 'Johann', 'Schneider', 'Hansli', 'm', '2010-10-09', 'Langstrasse 18', '8000', 'Zürich', 0, 1);
 INSERT INTO `users` VALUES(2, '1', 'heidi@example.com', '$2a$11$k8EkgulVtkQxuD8Jt.xyUeg1wNKivBHbluVyI98vnFpt9.SJsx1XK', 'Heidi', 'Mueller', 'Johann', 'Schneider', 'Heidi', 'm', '2017-09-10', 'Langstrasse 18', '8000', 'Zürich', 0, 1);
 INSERT INTO `users` VALUES(3, '12', 'NewTestUser123882A@exampl.com', '1ef9b6940d94bd487559e7ca4efd9e1a8b27ac9f920b67e3838f99b23e742ad7', 'nice USEreNAme', 'Blass', 'bl', 'adlöfjk', 'adsf', 'm', '1999-10-10', 'HAldenstrasse', '8000', 'Zürich', 0, 1);
+
+-- DATEN für CHAT
+
+DROP TABLE IF EXISTS `chat`;
+CREATE TABLE `chat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `client_uuid` varchar(255),
+  `user_id` varchar(255) NOT NULL,
+  `class_id` varchar(255),
+  `email` varchar(255),
+  `message` text NOT NULL,
+  `sent_at` date NOT NULL,
+  PRIMARY KEY (`id`)
+)  ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+
+
+INSERT INTO `chat` VALUES('1', 'X1', '1', '1', '', 'Lorem ipsum', '2016-10-09  16:14:07');
+INSERT INTO `chat` VALUES('2', 'X2', '2', '1', '', 'sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,', '2016-10-22  16:12:07');
