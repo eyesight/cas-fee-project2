5c536308132f23940312a2bcd5c2d50552a8a360
Andy Wildisen <awildisen@gmx.ch>
2017-11-18 10:43:47 +0100

    textares resizing delete code

--- src/app/_directives/textarea-autosize.directive.ts
+++ src/app/_directives/textarea-autosize.directive.ts
@@ -14,27 +14,8 @@ export class AppTextareaAutosizeDirective {
   private retries: number = 0;
   private textAreaEl: any;
   constructor(public element: ElementRef) {
-    if (this.element.nativeElement.tagName !== TEXTAREA_NAME) {
-      this._findNestedTextArea();
-
-    } else {
+    if (this.element.nativeElement.tagName === TEXTAREA_NAME) {
       this.textAreaEl = this.element.nativeElement;
-    }
-  }
-  // try to find textarea at the beginning
-  // restriction: there can only be 1 textare in the used element
-  _findNestedTextArea() {
-    this.textAreaEl = this.element.nativeElement.getElementsByTagName(TEXTAREA_NAME)[0];
-    if (!this.textAreaEl) {
-      if (this.retries >= MAX_LOOKUP_RETRIES) {
-        console.warn('autosize: textarea not found');
-
-      } else {
-        this.retries++;
-        setTimeout(() => {
-          this._findNestedTextArea();
-        }, 100);
-      }
     }
   }
 
