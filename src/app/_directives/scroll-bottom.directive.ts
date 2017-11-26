7fb2d244ed170890f255f5631c7685ecf4048272
Andy Wildisen <awildisen@gmx.ch>
2017-11-26 16:53:11 +0100

    chat: only scroll after having Send clicked

--- src/app/_directives/scroll-bottom.directive.ts
+++ src/app/_directives/scroll-bottom.directive.ts
@@ -1,14 +1,15 @@
 /**
  * Created by awedag on 20.11.17.
  */
-import {Directive, ElementRef, ViewChild, OnChanges, AfterContentInit, AfterContentChecked} from '@angular/core';
+import {Directive, ElementRef, OnChanges, AfterContentInit} from '@angular/core';
 
 
 @Directive({
-  selector: '[appScrollBottom]'
+  selector: '[appScrollBottom]',
+  exportAs: 'scrollBottom'
 })
 
-export class AppScrollBottomDirective implements AfterContentInit, OnChanges, AfterContentChecked {
+export class AppScrollBottomDirective implements AfterContentInit, OnChanges {
   // @ViewChild('scroll-bottom') private scrollBottom: ElementRef;
 
   private scrollBottom: any;
@@ -21,16 +22,27 @@ export class AppScrollBottomDirective implements AfterContentInit, OnChanges, Af
   public ngAfterContentInit() {
     this.scrollToBottom();
   }
-  public ngAfterContentChecked() {
-    this.scrollToBottom();
-  }
+
+  // to wait for AfterViewChecked or AfterContentChecked is causing it to update even there is no real change on the DOM
+  // which then dizzies the user
+  // public ngAfterContentChecked() {
+  //  this.scrollToBottom();
+  // }
   public ngOnChanges() {
     this.scrollToBottom();
   }
 
+  public scrollNow(){
+    this.scrollToBottom();
+  }
   private scrollToBottom(): void {
-    try {
-      this.element.nativeElement.scrollTop = this.element.nativeElement.scrollHeight;
-    } catch (err) { console.log('scrollToBottom Error:'+err);}
+    // using setTimeout allows to wait long enough to adjust scrolling
+    setTimeout(() => {
+      try {
+        this.element.nativeElement.scrollTop = this.element.nativeElement.scrollHeight;
+      } catch (err) {
+        console.log('scrollToBottom Error:' + err);
+      }
+    }, 200);
   }
 }
