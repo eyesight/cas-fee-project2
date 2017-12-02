6af41f7f0bc8871f6252c9ea2115e5989150a68b
Andy Wildisen <awildisen@gmx.ch>
2017-12-02 18:37:47 +0100

    adding classlist - html table breaks

--- src/app/classlist/classlist.component.ts
+++ src/app/classlist/classlist.component.ts
@@ -6,7 +6,7 @@ import { Klasse } from '../_models/klasse.model';
 
 
 @Component({
-  selector: 'app-classlistt',
+  selector: 'app-classlist',
   templateUrl: './classlist.component.html'
 })
 export class ClasslistComponent implements OnInit {
