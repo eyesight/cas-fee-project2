d462618d44be076c06bfbc073e35abf8839ab6ed
Andy Wildisen <awildisen@gmx.ch>
2017-12-14 18:07:25 +0100

    profil: affatar ...

--- src/app/profile/profile-avatar/profile-avatar.component.ts
+++ src/app/profile/profile-avatar/profile-avatar.component.ts
@@ -26,7 +26,7 @@ export class ProfileAvatarComponent {
   av = new Avatar;
   avatarObject = new UserAvatar;
   filename: string;
-  processedImages: any = [];
+  processedImage: any;
    images: Array<IImage> = [];
    fileList: FileList;
 
@@ -99,32 +99,13 @@ export class ProfileAvatarComponent {
       let reader = new FileReader();
       let files = event.target.files[0];
       let fileList = FileList;
-    //  console.log('in reader.onload 0:' + event.target.files[0]. imageDataUrl.length);
-      console.dir(files);
       reader.readAsDataURL(files);
 
-     //   let files =    Array.from(event.target.files);
-      console.log('fiels:');
+      console.log('fiels:' + event.target.files[0]);
 
-
-      //
-      // this.ng2ImgToolsService.resize(event.target.files[0], 100, 100).subscribe(result => {
-      //   //all good, result is a file
-      //   console.info(result);
-      // }, error => {
-      //   //something went wrong
-      //   //use result.compressedFile or handle specific error cases individually
-      // });
-
-
-      // this.ng2ImgMax.resizeImage(event.target.files[0], 100, 100).subscribe(
-      //   result => {
-      //   //  this.uploadedImage = new File([result], result.name);
-      //
-      // console.log('resize to 100px:' + result.length);
-      // console.dir(result);
-
-        /*  ImageCompressService.filesToCompressedImageSource(event.target.files).then(observableImages => {
+      // reset
+      this.images = [];
+         ImageCompressService.filesToCompressedImageSource(event.target.files).then(observableImages => {
             observableImages.subscribe((image) => {
               this.images.push(image);
               console.log('compression on success' );
@@ -132,30 +113,20 @@ export class ProfileAvatarComponent {
             }, (error) => {
               console.log("Error while converting");
             }, () => {
-              this.processedImages = this.images;
-              console.log('final on success:' +  this.images[0].compressedImage.imageDataUrl.length);
+              this.processedImage = this.images[0];
+              console.log('final on comporessed.length:' +  this.images[0].compressedImage.imageDataUrl.length + ' no comp:' + this.avatarUrl.length);
 
-              // this.av.filename = files.name;
-              // this.av.filetype = files.type;
-              //    this.av.value = event.target.result.split(',')[1];
-              //  this.av.value = this.images[0].compressedImage.imageDataUrl.split(',')[1];
-              // // this.nomalAvatar.avatar = this.av;
-              //
+              if (  this.images[0].compressedImage.imageDataUrl.length  < this.avatarUrl.length) {
+                this.av.value = this.images[0].compressedImage.imageDataUrl.split(',')[1];
+                this.nomalAvatar.avatar = this.av;
+              }
+              else {
+                  this.av.value  = this.avatarUrl;
+                this.nomalAvatar.avatar = this.av;
+              }
 
-              // this.showTitle = true;
             });
-          });*/
-
-      //   },
-      //   error => {
-      //     console.log('😢 Oh no!', error);
-      //   }
-      // )
-
-
-
-
-
+          });
 
       reader.onload = (event: any) => {
         this.previewUrl = event.target.result;
@@ -164,40 +135,10 @@ export class ProfileAvatarComponent {
 
         console.log('in reader.onload 110:' + this.avatarUrl.length);
 
-
-       /* ImageCompressService.filesToCompressedImageSource(event.target.files).then(observableImages => {
-          observableImages.subscribe((image) => {
-            this.images.push(image);
-            console.log('compression on success' );
-            //  console.dir(image.compressedImage.imageDataUrl);
-          }, (error) => {
-            console.log("Error while converting");
-          }, () => {
-            this.processedImages = this.images;
-            console.log('final on success:' +  this.images[0].compressedImage.imageDataUrl.length);
-
-            // this.av.filename = files.name;
-            // this.av.filetype = files.type;
-            //    this.av.value = event.target.result.split(',')[1];
-              this.av.value = this.images[0].compressedImage.imageDataUrl.split(',')[1];
-            this.nomalAvatar.avatar = this.av;
-            //
-
-            // this.showTitle = true;
-          });
-        });*/
-
-        //  this.nomalAvatar = ;
-        // const av = new Avatar;
         this.av.filename = files.name;
         this.av.filetype = files.type;
       //  console.dir(this.avatarUrl);
        this.av.value = this.avatarUrl;
-     //   this.av.value = this.images[0].imageDataUrl;
-
-        // console.log('final on afiilling' +  this.images[0].compressedImage.imageDataUrl.length);
-        //
-        // this.av.value = this.images[0].compressedImage.imageDataUrl;
 
         this.nomalAvatar.avatar = this.av;
 
@@ -205,17 +146,7 @@ export class ProfileAvatarComponent {
         console.log('in reader.onload 1');
        // console.dir(this.avatarUrl);
       };
-      /*  if(files){
-       reader.readAsDataURL(files);
-       console.log('in reader 2');
-       console.dir(reader);
-       this.avatar.setValue({
-       filename: files.name,
-       filetype: files.type,
-       value: reader.result.split(',')[1]
-       });
-       console.log(this.avatar.value);
-       } */
+
     }
   }
 }
