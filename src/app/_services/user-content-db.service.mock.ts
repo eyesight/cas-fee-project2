d45330fcf41233811013fd347a6d7b3fcd1c3b12
Andy Wildisen <awildisen@gmx.ch>
2017-12-18 17:28:20 +0100

    mock

---
+++ src/app/_services/user-content-db.service.mock.ts
@@ -0,0 +1,42 @@
+/**
+ * Created by awedag on 20.11.17.
+ */
+
+import {Injectable} from '@angular/core';
+import {StorageService, StorageKeys} from './storage.service';
+
+import {Http, Headers, RequestOptions, Response} from '@angular/http';
+import {appConfig} from '../_helpers/app.config';
+import {User} from '../_models/user.model';
+
+
+@Injectable()
+export class UserContentDbServiceMock {
+
+  private userContentCache: User = new User;
+
+  constructor(private storage: StorageService) {
+    console.log('User-UserContentDbService constructed');
+    this.userContentCache.email = 'test@test.com';
+
+  }
+
+  public saveCurrentUser(data: User) {
+    this.userContentCache = data;
+
+  }
+
+  public removeCurrentUser() {
+  }
+
+  public getCurrentUser(): User {
+    console.log('userContentGetCurrentUser:' + this.userContentCache);
+    if (!this.userContentCache) {
+      return this.userContentCache || null;
+    } else {
+      return this.userContentCache;
+    }
+  }
+
+
+}
