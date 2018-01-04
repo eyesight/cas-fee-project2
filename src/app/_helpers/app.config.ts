82ef29336d73202183b433e5c45c44c7d2cc15cc
Andy Wildisen <awildisen@gmx.ch>
2018-01-04 08:06:15 +0100

    testing

--- src/app/_helpers/app.config.ts
+++ src/app/_helpers/app.config.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 
 export const appConfig = {
-  // apiUrl: 'http://localhost:3020'
-apiUrl: 'http://192.168.1.114:3020'
+  apiUrl: 'http://localhost:3020'
+// apiUrl: 'http://192.168.1.114:3020'
 };
 
 @Injectable()
